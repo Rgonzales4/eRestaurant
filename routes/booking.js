dffcf9fe1908348a6e37a28b8e0b97ec7a7627ea
Kieren Karanjia <kieren.karanjia@gmail.com>
2021-03-31 12:25:45 +1100

    trying to make booking nav tab appear ;-;

--- routes/booking.js
+++ routes/booking.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  console.log('Menu page opened');
-  res.render('menu');
+  console.log('Booking page opened');
+  res.render('booking', { req: req });
 });
 
 module.exports = router;
+
