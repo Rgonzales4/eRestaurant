d9f6ced640bb8bea14352f06d29ae99ad3485156
Rgonzales4 <roshel.gonzales@yahoo.com>
2021-04-02 17:29:11 +1100

    Add Authentication to the booking page

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,44 +1,47 @@
 const express = require('express');
 const Booking = require('../models/booking');
-const User = require('../models/users')
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get('/', checkAuthenticated, async (req, res) => {
   console.log('Booking page opened');
-  const booking = await Booking.find().sort({bookingID : 'asc'});
-  res.render('booking', {req : req, booking : booking});
+  const booking = await Booking.find().sort({ bookingID: 'asc' });
+  res.render('booking', { req: req, booking: booking });
 });
 
-router.get('/createBooking', (req, res) => {
+router.get('/createBooking', checkAuthenticated, (req, res) => {
   console.log('create booking opened');
-  res.render('createBooking', { req : req, booking : new Booking() });
-})
+  res.render('createBooking', { req: req, booking: new Booking() });
+});
 
 router.post('/', async (req, res) => {
-
-let booking = new Booking({
+  let booking = new Booking({
     bookingID: req.body.bookingID,
     time: req.body.bookingDate,
     bookingNumber: req.body.bookingNumber,
     allergyDescription: req.body.allergyDescription,
     bookingUser: req.user.email,
-})  
-let confirmBooking = await Booking.findOne({bookingID: req.body.bookingID})
-if(confirmBooking){
-  res.render('createBooking', {req: req, booking : booking})
-  console.log("This booking already exists")
-}
-else{
-  booking = await booking.save()
-  console.log("booking saved to databases")
-  res.render('createBooking', {req: req, booking : booking} )
-}})
+  });
+  let confirmBooking = await Booking.findOne({ bookingID: req.body.bookingID });
+  if (confirmBooking) {
+    res.render('createBooking', { req: req, booking: booking });
+    console.log('This booking already exists');
+  } else {
+    booking = await booking.save();
+    console.log('booking saved to databases');
+    res.render('createBooking', { req: req, booking: booking });
+  }
+});
 
 router.get('/removeBooking', (req, res) => {
   console.log('remove booking opened');
-  res.render('removeBooking', {req : req});
-})
+  res.render('removeBooking', { req: req });
+});
 
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
 
 module.exports = router;
-
