d2e14272bef79241b4a3a0daad559ce2426516b9
Rgonzales4 <roshel.gonzales@yahoo.com>
2021-04-02 17:16:03 +1100

    Tweak Database

--- routes/database.js
+++ routes/database.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../models/users');
+const Booking = require('../models/booking');
 
 router.get('/', checkAdmin, async (req, res) => {
   console.log('Databasee page opened');
   const Users = await User.find().sort({ firstName: 'asc' });
-  res.render('database', { req: req, Users: Users });
+  const Bookings = await Booking.find().sort({ bookingID: 'asc' });
+  res.render('database', { req: req, Users: Users, Bookings: Bookings });
 });
 
 function checkAdmin(req, res, next) {
