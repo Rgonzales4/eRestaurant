dc56810f15d3fb30c086335c21692e87b562a7fc
Rgonzales4 <roshel.gonzales@yahoo.com>
2021-04-10 16:03:24 +1000

    Admin function to delete users in progress

--- routes/database.js
+++ routes/database.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const User = require('../models/users');
 const Booking = require('../models/booking');
+const { Model } = require('mongoose');
 
 router.get('/', checkAdmin, async (req, res) => {
   console.log('Database page opened');
@@ -13,9 +14,20 @@ router.get('/', checkAdmin, async (req, res) => {
 
 router.get('/account/:userId', checkAdmin, async (req, res) => {
   const userAccount = await User.findOne({ userId: req.params.userId });
-  console.log(`Accessing user profile: ${userAccount.firstName} ${userAccount.lastName}`);
+  console.log(
+    `Accessing user profile: ${userAccount.firstName} ${userAccount.lastName}`
+  );
   res.render('profile', { req: req, user: userAccount });
 });
+
+//NOT YET FINISHED - ITS DELETING THE WRONG USER
+// router.delete('/:userId', checkAdmin, async (req, res) => {
+//   // const userAccount = await User.findOne({ userId: req.params.userId });
+//   await User.findOneAndDelete(req.params.userId);
+//   const Users = await User.find().sort({ firstName: 'asc' });
+//   const Bookings = await Booking.find().sort({ bookingID: 'asc' });
+//   res.render('database', { req: req, Users: Users, Bookings: Bookings });
+// });
 
 function checkAdmin(req, res, next) {
   if (req.isAuthenticated() && req.user.isAdmin === true) {
