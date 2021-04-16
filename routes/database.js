903a15420b5b51c028bdb005cc5f039b3ede841c
Kieren Karanjia <kieren.karanjia@gmail.com>
2021-04-17 00:13:03 +1000

    Most of menu created, namely, the model, view, and database page, however you cannot create or remove menu items yet

--- routes/database.js
+++ routes/database.js
@@ -3,12 +3,14 @@ const router = express.Router();
 
 const User = require('../models/users');
 const Booking = require('../models/booking');
+const MenuItem = require('../models/menu_item');
 
 router.get('/', checkAdmin, async (req, res) => {
   console.log('Databasee page opened');
   const Users = await User.find().sort({ firstName: 'asc' });
   const Bookings = await Booking.find().sort({ bookingID: 'asc' });
-  res.render('database', { req: req, Users: Users, Bookings: Bookings });
+  const MenuItems = await MenuItem.find().sort({ itemID: 'asc' });
+  res.render('database', { req: req, Users: Users, Bookings: Bookings, MenuItems: MenuItems });
 });
 
 function checkAdmin(req, res, next) {
