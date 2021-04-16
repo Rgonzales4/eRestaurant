64f7620d5832abde045389dd87428cddd110da91
Rgonzales4 <roshel.gonzales@yahoo.com>
2021-04-16 14:10:17 +1000

    User can successfully edit their account

--- routes/profile.js
+++ routes/profile.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const { syncIndexes } = require('../models/users');
 const router = express.Router();
 
 const User = require('../models/users');
 
 router.get('/', checkAuthenticated, async (req, res) => {
   const userAccount = await User.findOne({ email: req.user.email });
-  console.log(`User ${userAccount.userId} page open`);
+  console.log(
+    `User ${userAccount.userId} ${userAccount.firstName} ${userAccount.lastName} page open`
+  );
   res.render('profile', { req: req, user: userAccount });
+});
+
+router.get('/editProfile', checkAuthenticated, async (req, res) => {
+  const userAccount = await User.findOne({ email: req.user.email });
+  console.log(
+    `Edit User ${userAccount.userId} ${userAccount.firstName} ${userAccount.lastName} page open`
+  );
+  res.render('editProfile', { req: req, user: userAccount });
+});
+
+router.post('/editProfile', checkAuthenticated, async (req, res) => {
+  const filter = { email: req.user.email };
+  const update = { firstName: req.body.firstName, lastName: req.body.lastName };
+  console.log(update);
+  await User.findOneAndUpdate(filter, update);
+  res.redirect('/profile');
 });
 
 function checkAuthenticated(req, res, next) {
