722c62844bed42b11e12cb86104500e53da14159
Rgonzales4 <roshel.gonzales@yahoo.com>
2021-05-17 00:38:36 +1000

    Git back on laptop

--- routes/profile.js
+++ routes/profile.js
@@ -1,55 +1,55 @@
-const express = require('express');
-const { syncIndexes } = require('../models/users');
+const express = require("express");
+const { syncIndexes } = require("../models/users");
 const router = express.Router();
 
-const User = require('../models/users');
+const User = require("../models/users");
 
-router.get('/', checkAuthenticated, async (req, res) => {
+router.get("/", checkAuthenticated, async (req, res) => {
   const userAccount = await User.findOne({ email: req.user.email });
   console.log(
     `User ${userAccount.userId} ${userAccount.firstName} ${userAccount.lastName} page open`
   );
-  res.render('profile', { req: req, user: userAccount, adminEdit: false });
+  res.render("profile", { req: req, user: userAccount, adminEdit: false });
 });
 
-router.get('/editProfile', checkAuthenticated, async (req, res) => {
+router.get("/editProfile", checkAuthenticated, async (req, res) => {
   const userAccount = await User.findOne({ email: req.user.email });
   console.log(
     `Edit User ${userAccount.userId} ${userAccount.firstName} ${userAccount.lastName} page open`
   );
-  res.render('editProfile', { req: req, user: userAccount, adminEdit: false });
+  res.render("editProfile", { req: req, user: userAccount, adminEdit: false });
 });
 
 //UPDATING THE PROFILE FUNCTION
-router.post('/editProfile', checkAuthenticated, async (req, res) => {
+router.post("/editProfile", checkAuthenticated, async (req, res) => {
   const filter = { email: req.user.email };
   const update = { firstName: req.body.firstName, lastName: req.body.lastName };
   console.log(update);
   await User.findOneAndUpdate(filter, update);
-  res.redirect('/profile');
+  res.redirect("/profile");
 });
 
 //DELETING THE USER ACCOUNT
-router.get('/deleteProfile', checkAuthenticated, async (req, res) => {
+router.get("/deleteProfile", checkAuthenticated, async (req, res) => {
   console.log(`Deleting user ${req.user.firstName} from the database`);
   const userToDelete = req.user.email;
   await User.findOneAndDelete({ email: userToDelete });
   req.logout();
-  res.redirect('/');
+  res.redirect("/");
 });
 
 function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect('/login');
+  res.redirect("/login");
 }
 
 function checkAdmin(req, res, next) {
   if (req.isAuthenticated() && req.user.isAdmin === true) {
     return next();
   }
-  res.redirect('/');
+  res.redirect("/");
 }
 
 module.exports = router;
